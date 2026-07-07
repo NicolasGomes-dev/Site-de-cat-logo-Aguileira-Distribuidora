7b5a65ab2a9344c6f2128bd0646ac6fe86834b6d
Nicolas <gomes160107@gmail.com>
2026-07-07 13:42:19 -0300

    Adiciona logos dos fabricantes da linha leve

--- lib/leve-products.ts
+++ lib/leve-products.ts
@@ -313,6 +313,280 @@ export const lightProducts = [
             {
                 name: "Timken",
                 logo: "/brands/timken.png"
+            },
+
+            {
+                name: "albarus",
+                logo: "/brands/albarus.png"
+            },
+
+            {
+                name: "Ampri",
+                logo: "/brands/ampri.png"
+            },
+
+            {
+                name: "Anroi",
+                logo: "/brands/anroi.jpg"
+            },
+
+            {
+                name: "Aplic",
+                logo: "/brands/aplic.png"
+            },
+
+            {
+                name: "Borgwarner",
+                logo: "/brands/borgwarner.png"
+            },
+
+            {
+                name: "Bosch",
+                logo: "/brands/bosch.webp"
+            },
+
+            {
+              name: "Bovenau",
+              logo: "/brands/bovenau.jpg"  
+            },
+
+            {
+                name: "Butuem",
+                logo: "/brands/butuem.jpg"
+            },
+
+            {
+                name: "Cabovel",
+                logo: "/brands/cabovel.png"
+            },
+
+            {
+                name: "Car 80",
+                logo: "/brands/car80.jpg"
+            },
+
+            {
+                name: "Cobreq",
+                logo: "/brands/cobreq.jpg"
+            },
+
+            {
+                name: "Cofap",
+                logo: "/brands/cofap.png"
+            },
+
+            {
+                name: "Controil",
+                logo: "/brands/controil.png"
+            },
+
+            {
+                name: "Driveway",
+                logo: "/brands/driveway.png"
+            },
+
+            {
+                name: "Dyna",
+                logo: "/brands/dyna.png"
+            },
+
+            {
+                name: "Eaton",
+                logo: "/brands/eaton.png"
+            },
+
+            {
+                name: "Elrling",
+                logo: "/brands/elrling.png"
+            },
+            {
+                name: "Flaus",
+                logo: "/brands/flaus.png"
+            },
+
+            {
+                name: "Ford",
+                logo: "/brands/ford.png"
+            },
+
+            {
+                name: "Frasle",
+                logo: "/brands/frasle.png"
+            },
+
+            {
+                name: "Fremax",
+                logo: "/brands/fremax.png"
+            },
+
+            {
+                name: "Garret",
+                logo: "/brands/garret.png"
+            },
+
+            {
+                name: "Gimacco",
+                logo: "/brands/gimacco.jpg"
+            },
+
+            {
+                name: "Hella",
+                logo: "/brands/hella.png"
+            },
+
+            {
+                name: "Hipper Freios",
+                logo: "/brands/hipperfreios.png"
+            },
+
+            {
+                name: "IBTF",
+                logo: "/brands/ibtf.jpg"
+            },
+
+            {
+                name: "igasa",
+                logo: "/brands/igasa.jpg"
+            },
+
+            {
+                name: "IMCE",
+                logo: "/brands/imce.jpg"
+            },
+
+            {
+                name: "Indisa",
+                logo: "/brands/indisa.png"
+            },
+
+            {
+                name: "Lemforder",
+                logo: "/brands/lemforder.png"
+            },
+
+            {
+                name: "Lonaflex",
+                logo: "/brands/lonaflex.jpg"
+            },
+
+            {
+                name: "Magneti Marelli",
+                logo: "/brands/magnetimarelli.png"
+            },
+
+            {
+                name: "Mahle",
+                logo: "/brands/mahle.jpg"
+            },
+
+            {
+                name: "Mirador",
+                logo: "/brands/mirador.png"
+            },
+
+            {
+                name: "Modefer",
+                logo: "/brands/modefer.jpg"
+            },
+
+            {
+                name: "Monroe",
+                logo: "/brands/monroe.png"
+            },
+
+            {
+                name: "Nakata",
+                logo: "/brands/nakata.jpg"
+            },
+
+            {
+                name: "Nytron",
+                logo: "/brands/nytron.png"
+            },
+
+            {
+                name: "Riosulense",
+                logo: "/brands/riosulense.png"
+            },
+            
+            {
+                name: "Roltens",
+                logo: "/brands/roltens.png"
+            },
+
+            {
+                name: "Sachs",
+                logo: "/brands/sachs.png"
+            },
+
+            {
+                name: "Schadek",
+                logo: "/brands/schadek.png"
+            },
+
+            {
+                name: "Spicer",
+                logo: "/brands/spicer.jpg"
+            },
+
+            {
+                name: "Takao",
+                logo: "/brands/takao.png"
+            },
+
+            {
+                name: "Tampas click",
+                logo: "/brands/tampasclick.jpg"
+            },
+
+            {
+                name: "THREE BOND",
+                logo: "/brands/threebond.png"
+            },
+
+            {
+                name: "TRW",
+                logo: "/brands/trw.png"
+            },
+
+            {
+                name: "Trimgo",
+                logo: "/brands/trimgo.jpg"
+            },
+
+            {
+                name: "Valclei",
+                logo: "/brands/valclei.png"
+            },
+
+            {
+                name: "Valeo",
+                logo: "/brands/valeo.png"
+            },
+            
+            {
+                name: "Man",
+                logo: "/brands/man.png"
+            },
+
+            {
+                name: "Wahler",
+                logo: "/brands/wahler.png"
+            },
+
+            {
+                name: "WD-40",
+                logo: "/brands/wd40.png"
+            },
+
+            {
+                name: "ZF direção",
+                logo: "/brands/zfdirecao.png"
+            },
+
+            {
+                name: "Urba Brosol",
+                logo: "/brands/urbabrosol.jpg"
             }
 
         ],
