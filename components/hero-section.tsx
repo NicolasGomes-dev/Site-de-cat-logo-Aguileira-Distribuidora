32696d68b24bc1f02398789e0aa02509f830f2c6
Nicolas <gomes160107@gmail.com>
2026-07-20 14:05:25 -0300

    Configurando responsividade do site

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -7,11 +7,9 @@ export function HeroSection() {
     <section className="bg-slate-100">
 
       {/* Conteúdo Principal */}
-      <div className="mx-auto flex max-w-screen-2xl items-start gap-16 px-6 py-20">
-
+      <div className="mx-auto flex max-w-screen-2xl flex-col-reverse items-center gap-10 px-6 py-12 lg:flex-row lg:items-start lg:gap-16 lg:py-20">
         {/* Texto */}
-        <div className="w-[40%]">
-
+        <div className="w-full lg:w-[40%]">
           <span className="rounded-full bg-brand-red/10 px-4 py-2 text-sm font-semibold text-brand-red">
             Há mais de 30 anos no mercado
           </span>
@@ -48,7 +46,7 @@ export function HeroSection() {
         </div>
 
         {/* Banner */}
-        <div className="w-[60%]">
+        <div className="w-full lg:w-[60%]">
           <HeroCarousel />
         </div>
 
