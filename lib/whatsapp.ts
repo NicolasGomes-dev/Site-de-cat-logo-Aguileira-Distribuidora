a063e6e42b3549a12d08d06f34f299a2850c057e
Nicolas <gomes160107@gmail.com>
2026-08-17 08:50:02 -0300

    Corrigindo popup de redirecionamento para o WhatsApp

--- lib/whatsapp.ts
+++ lib/whatsapp.ts
@@ -1,22 +1,22 @@
 export const whatsappCompanies = [
   {
-    name: "Cuiabá",
-    description: "Atendimento comercial",
-    phone: "556596984127",
+    name: "Para",
+    description: "91 8250-1220",
+    phone: "559182501220",
   },
   {
-    name: "Goiânia",
-    description: "Atendimento comercial",
-    phone: "556295128248",
-  },
-  {
-    name: "Palmas",
-    description: "Atendimento comercial",
+    name: "Araguaina",
+    description: "63 9229-7936",
     phone: "556392297936",
   },
   {
-    name: "Belém",
-    description: "Atendimento comercial",
-    phone: "559182501220",
+    name: "Cuiaba",
+    description: "65 9698-4127",
+    phone: "556596984127",
+  },
+  {
+    name: "Goiania",
+    description: "62 9512-8248",
+    phone: "556295128248",
   },
 ]
